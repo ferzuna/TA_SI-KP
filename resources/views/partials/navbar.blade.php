65c45e71176ef14d6b52742487220fb290f2b85e
ferzuna <fadzil324@gmail.com>
2023-01-02 11:57:53 +0700

    navbar sudah selesai

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -6,7 +6,7 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav mb-2 mb-lg-0">
                 <li class="nav-item">
                     <a class="nav-link" aria-current="page" href="#">Permohonan KP</a>
                 </li>
@@ -14,10 +14,11 @@
                     <a class="nav-link" href="#">Dokumen KP</a>
                 </li>
             </ul>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+            <div class="btn-login">
+                <button type="button" class="btn btn-light" style="border-radius: 24px">
+                    <div style="padding: 0px 10px; font-size: 14px; font-weight: 600">Login</div>
+                </button>
+            </div>
         </div>
     </div>
 </nav>
