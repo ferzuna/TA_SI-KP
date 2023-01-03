97a760e12a7c2c5ecbf3c50f6c345304e8d02705
silumankadal <63130587+silumankadal@users.noreply.github.com>
2023-01-03 09:58:23 +0700

    role

---
+++ app/Http/Controllers/KoorController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Koor;
+use App\Http\Requests\StoreKoorRequest;
+use App\Http\Requests\UpdateKoorRequest;
+
+class KoorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreKoorRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreKoorRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Koor  $koor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Koor $koor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Koor  $koor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Koor $koor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateKoorRequest  $request
+     * @param  \App\Models\Koor  $koor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateKoorRequest $request, Koor $koor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Koor  $koor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Koor $koor)
+    {
+        //
+    }
+}
