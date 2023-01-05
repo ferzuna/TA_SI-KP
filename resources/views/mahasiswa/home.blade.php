271bc789663a6166c19592fa8b8a32ec45249d3f
ferzuna <fadzil324@gmail.com>
2023-01-05 19:47:36 +0700

    page mahasiswa belum selesai

--- resources/views/mahasiswa/home.blade.php
+++ resources/views/mahasiswa/home.blade.php
@@ -1,5 +1,131 @@
 @extends('mahasiswa.layouts.main')
 
-@section('content')
-    <h1>halaman mahasiswa</h1>
+@section('section')
+    <section class="welcome-mahasiswa">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12 col-sm-12 col-md-12">
+                    <div class="banner">
+                        <div class="media-left">
+
+                        </div>
+                        <div class="media-right">
+                            selamat datang Fadzil, di sistem informasi KP
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="biodata-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="biodata-card">
+                            <div class="card">
+                                <div class="card-content">
+                                    <div class="card-header">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-md-4">
+                                                    <img src="https://www.w3schools.com/howto/img_avatar.png"
+                                                        style="max-width: 150px; width: 100%" alt="foto">
+                                                </div>
+                                                <div class="col-md-8">
+                                                    <h4><b>Mahasiswa</b></h4>
+                                                    <span>Fadzil Ferdiawan</span>
+                                                    <br>
+                                                    <span>Angkatan 2019</span>
+                                                    <br>
+                                                    <span>0812939491929</span>
+                                                    <br>
+                                                    <span>fadzilferdiawan@students.undip.ac.id</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="card-footer text-center">
+                                            <span>Departemen Teknik Komputer
+                                            </span>
+                                            <br>
+                                            <span>Teknik Komputer S1</span>
+                                            <br>
+                                            <span>Semester 8</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-8">
+                        <div class="banner">
+                            <div class="media-left">
+
+                            </div>
+                            <div class="media-right">
+                                Mahasiswa mencari tempat/lokasi Kerja Praktek, setelah diterima Mahasiswa mengajukan Surat
+                                Permohonan Kerja Praktek dan proposal
+                                pengajuan pelaksanaan Kerja Praktek ke Dosen Pembimbing
+                            </div>
+                        </div>
+                        <div class="banner">
+                            <div class="media-left">
+
+                            </div>
+                            <div class="media-right">
+                                Mahasiswa mencari tempat/lokasi Kerja Praktek, setelah diterima Mahasiswa mengajukan Surat
+                                Permohonan Kerja Praktek dan proposal
+                                pengajuan pelaksanaan Kerja Praktek ke Dosen Pembimbing
+                            </div>
+                        </div>
+                        <div class="banner">
+                            <div class="media-left">
+
+                            </div>
+                            <div class="media-right">
+                                Mahasiswa mencari tempat/lokasi Kerja Praktek, setelah diterima Mahasiswa mengajukan Surat
+                                Permohonan Kerja Praktek dan proposal
+                                pengajuan pelaksanaan Kerja Praktek ke Dosen Pembimbing
+                            </div>
+                        </div>
+                        <div class="banner">
+                            <div class="media-left">
+
+                            </div>
+                            <div class="media-right">
+                                Mahasiswa mencari tempat/lokasi Kerja Praktek, setelah diterima Mahasiswa mengajukan Surat
+                                Permohonan Kerja Praktek dan proposal
+                                pengajuan pelaksanaan Kerja Praktek ke Dosen Pembimbing
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        {{-- Menu Mahasiswa --}}
+        <div class="menu-mahasiswa">
+            <div class="container-fluid">
+                <h2 class="text-center">
+                    Layanan
+                </h2>
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="card">
+                            <h2 class="text-center">Test</h2>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="card">
+                            <h2 class="text-center">Test</h2>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="card">
+                            <h2 class="text-center">Test</h2>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
--- resources/views/mahasiswa/layouts/main.blade.php
+++ resources/views/mahasiswa/layouts/main.blade.php
@@ -6,18 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- untuk css ini di folder public ya -->
-    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.rtl.min.css"
         integrity="sha384-DOXMLfHhQkvFFp+rWTZwVlPVqdIhpDVYT9csOnHSgWQWPX0v5MCGtjCJbY6ERspU" crossorigin="anonymous">
     <title>Kerja Praktik - Teknik Komputer</title>
 
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/mahasiswa.css') }}">
 </head>
 
 <body>
     @include('mahasiswa.layouts.navbar')
 
-    <div>
+    <div class="app-mahasiswa">
         @yield('section')
     </div>
 
