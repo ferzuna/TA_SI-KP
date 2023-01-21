54d36867f939125a0dec9b8442be5847a60cb458
silumankadal <63130587+silumankadal@users.noreply.github.com>
2023-01-21 14:12:54 +0700

    berkas

---
+++ database/migrations/2023_01_21_070307_create_bimbingans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBimbingansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bimbingans', function (Blueprint $table) {
+            $table->id();
+            $table->string('NIP');
+            $table->string('NIM');
+            $table->string('status');
+            $table->string('makalah');
+            $table->string('laporan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bimbingans');
+    }
+}
---
+++ database/migrations/2023_01_21_070345_create_penilaians_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePenilaiansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('penilaians', function (Blueprint $table) {
+            $table->id();
+            $table->string('NIP');
+            $table->string('NIM');
+            $table->string('berkasnilaiakhir');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('penilaians');
+    }
+}
