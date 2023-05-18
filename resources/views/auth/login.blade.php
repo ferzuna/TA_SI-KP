e8f773a1e8424182a2d9f75b12b843db17d079f7
silumankadal <63130587+silumankadal@users.noreply.github.com>
2023-05-18 11:42:46 +0700

    achievement' login menggunakan username' unlocked

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,14 +24,14 @@
 
                                     <div class="row mb-3">
                                         <label for="email"
-                                            class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                                            class="col-md-4 col-form-label text-md-end">{{ __('Username or Email') }}</label>
 
                                         <div class="col-md-6">
-                                            <input id="email" type="email"
-                                                class="form-control @error('email') is-invalid @enderror" name="email"
-                                                value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                            <input id="usename" type="text"
+                                                class="form-control @error('username') is-invalid @enderror" name="username"
+                                                value="{{ old('username') }}" required autofocus>
 
-                                            @error('email')
+                                            @error('username')
                                                 <span class="invalid-feedback" role="alert">
                                                     <strong>{{ $message }}</strong>
                                                 </span>
